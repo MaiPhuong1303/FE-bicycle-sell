422613ce6387c5fd666bbfc130e9c65d1d2543a8
minhthu <21130198@st.hcmuaf.edu.vn>
2024-06-22 17:07:36 +0700

    instruct

--- src/components/layouts/components/Header/NavBar/navbar.tsx
+++ src/components/layouts/components/Header/NavBar/navbar.tsx
@@ -7,6 +7,7 @@ import images from '../../../../../images/images';
 import config from "../../../../../config/config";
 
 
+
 const cx = classNames.bind(styles);
 
 function NavBar() {
@@ -43,6 +44,8 @@ function NavBar() {
                     />
                     <MenuItem title="Giới thiệu" to={config.routes.about} />
                     <MenuItem title="Liên hệ" to={config.routes.contact} />
+                    <MenuItem title="Hướng dẫn mua hàng" to={config.routes.instruct} />
+
                 </Menu>
             </div>
         </div>
---
+++ src/pages/Instruct/index.tsx
@@ -0,0 +1,16 @@
+import Home from "../Home";
+import classNames from "classnames/bind";
+import styles from "../../components/layouts/components/Header/NavBar/Menu/Submenu/Submenu.module.scss";
+const cx = classNames.bind(styles);
+function Instruct () {
+    return (
+
+        <div className={cx('container')}>
+
+        </div>
+    );
+
+}
+
+export default Instruct;
+
