c42e0a1e65c8ba7f9b745dd4f917ff6fc78d8c67
MaiPhuonghuynh <149395633+MaiPhuonghuynh@users.noreply.github.com>
2024-06-24 23:19:32 +0700

    Tạo fake server sử dụng json-server và làm skeletons cho danh sách sản phẩm

---
+++ src/data/api/axios.ts
@@ -0,0 +1,23 @@
+// import axios from 'axios';
+//
+// const BASE_URL = 'http://localhost:4000';
+//
+// axios.get(`${BASE_URL}/products`)
+//     .then(response => {
+//         console.log(response.data); // Log dữ liệu sản phẩm từ fake server
+//     })
+//     .catch(error => {
+//         console.error('Error fetching products:', error);
+//     });
+// api/axiosInstance.ts
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:4000';
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000, // Thời gian chờ tối đa (nếu cần)
+    // Cấu hình khác nếu có
+});
+
+export default axiosInstance;
