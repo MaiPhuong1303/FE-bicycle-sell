a8fa35cc669ff4b8aa994b3fa939ee2d2512707e
BuiThiThuyTrang <168885695+BuiThiThuyTrang@users.noreply.github.com>
2024-07-21 02:47:37 +0700

    Trang thanh toán

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,52 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import DefaultLayout from './components/layouts/DefaultLayout';
-import {publicRoutes} from './routes';
-import {ShoppingContextProvider} from "./components/contexts/ShoppingContext";
-import Checkout from "./pages/Checkout/Checkout";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./components/layouts/DefaultLayout";
 
-import {DarkModeProvider} from './components/layouts/components/darkMode/DarkModeContext';
+import { publicRoutes } from "./routes";
+import { ShoppingContextProvider } from "./components/contexts/ShoppingContext";
+import Checkout from "./pages/Checkout/Checkout";
+import Collection from "./pages/Collection/Collection"; // Import trang Collection
+import OrderConfirmation from "./pages/OrderConfirmation/OrderConfirmation"; // Import trang xác nhận đơn hàng
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import { DarkModeProvider } from "./components/layouts/components/darkMode/DarkModeContext";
 
 function App() {
-    return (
-        <DarkModeProvider>
-            <ShoppingContextProvider> {/* Bao bọc ứng dụng với ShoppingContextProvider */}
-
-                <Router>
-                    <div className="App">
-                        <Routes>
-                            {publicRoutes.map((route, index) => {
-                                const Page = route.component;
-                                return (
-                                    <Route
-                                        key={index}
-                                        path={route.path}
-                                        element={
-
-                                            <DefaultLayout>
-
-                                                <Page/>
-
-                                            </DefaultLayout>
-
-                                        }
-                                    />
-                                );
-                            })}
-                            <Route path="/checkout" element={<Checkout/>}/>
-                        </Routes>
-                    </div>
-                </Router>
-
-            </ShoppingContextProvider>
-        </DarkModeProvider>
-    );
+  return (
+    <DarkModeProvider>
+      <ShoppingContextProvider> {/* Bao bọc ứng dụng với ShoppingContextProvider */}
+        <Router>
+          <div className="App">
+            <Routes>
+              {publicRoutes.map((route, index) => {
+                const Page = route.component;
+                return (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    element={
+                      <DefaultLayout>
+                        <Page />
+                      </DefaultLayout>
+                    }
+                  />
+                );
+              })}
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/collection" element={<Collection />} />{" "}
+              {/* Đường dẫn đến Collection */}
+              <Route
+                path="/order-confirmation"
+                element={<OrderConfirmation />}
+              />{" "}
+              {/* Đường dẫn đến OrderConfirmation */}
+            </Routes>
+          </div>
+        </Router>
+      </ShoppingContextProvider>
+    </DarkModeProvider>
+  );
 }
 
 export default App;
