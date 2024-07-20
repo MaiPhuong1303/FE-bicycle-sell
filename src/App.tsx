513dc3b35a0566c49e73e6604dec0f6de7647b62
BuiThiThuyTrang <168885695+BuiThiThuyTrang@users.noreply.github.com>
2024-07-20 15:15:22 +0700

    Xử lý giỏ hàng

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,37 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import DefaultLayout from './components/layouts/DefaultLayout';
 import {publicRoutes} from './routes';
+import { ShoppingContextProvider} from "./components/contexts/ShoppingContext";
+import Checkout from "./pages/Checkout/Checkout";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.component;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <DefaultLayout>
-                                        <Page/>
-                                    </DefaultLayout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-
-            </div>
-        </Router>
+        <ShoppingContextProvider> {/* Bao bọc ứng dụng với ShoppingContextProvider */}
+            <Router>
+                <div className="App">
+                    <Routes>
+                        {publicRoutes.map((route, index) => {
+                            const Page = route.component;
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <DefaultLayout>
+                                            <Page/>
+                                        </DefaultLayout>
+                                    }
+                                />
+                            );
+                        })}
+                        <Route path="/checkout" element={<Checkout />} />
+                    </Routes>
+                </div>
+            </Router>
+        </ShoppingContextProvider>
     );
 }
 
